7fce73f687cdd69952d950f5981cf425b7346d13
hannah <hannah.greiner@mymail.champlain.edu>
2019-11-08 11:59:04 -0500

    in class UI work

--- UI.py
+++ UI.py
@@ -37,28 +37,61 @@ class MainUI:
         if cmds.windowPref(self.window, exists=True):
             cmds.windowPref(self.window, remove=True)
 
-        self.typeWin = cmds.window(self.window, title="UV Animation Timeline",
+        self.typeWin = cmds.window(self.window, title="Animation Timeline",
                                    minimizeButton=False, maximizeButton=False, sizeable=False)
 
     def baseUI(self):
         cmds.columnLayout(self.column, parent=self.typeWin)
 
         cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, self.width)], parent=self.column)
-        cmds.button(label="BUILD SCENE", command=lambda args: CreateBuild().buildObjects())
+        cmds.text("STEP ONE", align="center", bgc=[0.5, 0.5, 0.5], h=30)
+        cmds.separator()
+
+        cmds.button(label="Build Skeleton", command=lambda args: CreateBuild().buildObjects())
+
+        cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, self.width)], parent=self.column)
+        cmds.separator()
+        cmds.text("STEP TWO: Animation Speed", align="center", bgc=[0.5, 0.5, 0.5], h=30)
+        cmds.separator()
         cmds.intSliderGrp("frameNum", label="Number of Frames", field=True,
                           minValue=MIN_TIME, maxValue=MAX_TIME, value=MIN_TIME,
                           columnWidth=[(1, 100), (2, 50), (3, WIDTH-125)],  cal=[1, "center"])
-        cmds.floatSliderGrp("deltaScale", label="Scale", field=True,
+
+        cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, self.width)], parent=self.column)
+        cmds.separator()
+        cmds.text("STEP THREE: Animation Scale", align="center", bgc=[0.5, 0.5, 0.5], h=30)
+        cmds.separator()
+        cmds.floatSliderGrp("deltaScale", label="Scale (current)", field=True,
                             minValue=0, maxValue=1, value=0.5, s=0.01,
                             columnWidth=[(1, 100), (2, 50), (3, WIDTH-125)],  cal=[1, "center"])
 
+        cmds.rowColumnLayout(numberOfColumns=2, columnWidth=[(1, self.width/4.0), (2, self.width/4.0 * 3)], parent=self.column)
+        cmds.text("Legs Scale", h=20)
+        cmds.floatSlider("legScale", minValue=0, maxValue=1, value=0.5, s=0.01)
+        cmds.text("Arms Scale", h=20)
+        cmds.floatSlider("armScale", minValue=0, maxValue=1, value=0.5, s=0.01)
+        cmds.text("Tilt Scale", h=20)
+        cmds.floatSlider("tiltScale", minValue=0, maxValue=1, value=0.5, s=0.01)
+
         cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, self.width)], parent=self.column)
-        cmds.button(label="PLAY", command=lambda args: Play().forwards())
+        cmds.separator()
+        cmds.text("STEP FOUR: Skeleton Size", align="center", bgc=[0.5, 0.5, 0.5], h=30)
+        cmds.separator()
+
+        cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, self.width)], parent=self.column)
+        cmds.separator()
+        cmds.text("STEP Five: Start", align="center", bgc=[0.5, 0.5, 0.5], h=30)
+        cmds.separator()
+        cmds.rowColumnLayout(numberOfColumns=1, columnWidth=[(1, self.width)], parent=self.column)
+        cmds.button(label="play", command=lambda args: Play().forwards())
+        cmds.text("", h=2)
         # cmds.button(label="REVERSE", command=lambda args: Play().backwards())
-        cmds.button(label="STOP", command=lambda args: Play().stop())
-        cmds.button(label="RESET", command=lambda args: reset())
-        cmds.button(label="QUIT", command=lambda args: end())
-        cmds.button(label="DEMO", command=lambda args: openMayaStuff.demo())
+        cmds.button(label="stop", command=lambda args: Play().stop())
+        cmds.text(" ", h=2)
+        cmds.button(label="reset", command=lambda args: reset())
+        cmds.text("  ", h=2)
+        cmds.button(label="quit", command=lambda args: end())
+        # cmds.button(label="DEMO", command=lambda args: openMayaStuff.demo())
         cmds.showWindow(self.window)
 
 
